f34611a74cb27a8b87b4fc431f045490e04a4cd6
Thundros <32390102+Thundros@users.noreply.github.com>
2019-12-11 11:26:51 -0600

    update...

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 
 	window.onload = function ( ) {
+
 		this.stats = new Stats ( );
 		document.body.appendChild ( this.stats.dom );
 		this.__game = new Phaser.Game ( this.__config );
 		window.focus ( );
+
 	}
 
 
